09ae67102581869119a5e92aefceac99535b9a66
Efazul Fayad <efazul.fayad1717@gmail.com>
2023-02-11 15:29:09 +0600

    pushed

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import Nav from '../components/nav'
+import Feature from '../components/feature'
+import Comprice from '../components/pricing/comprce'
+import Footer from '../components/footer'
+import Panel from '../components/panel'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Sohag Technology - High Speed Internet</title>
+        <meta name="description" content="Leading ISP in Bangladesh" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <>
+        <Nav />
+        <Panel />
+        <Feature />
+        <Comprice />
+      </>
+      <Footer />
+    </div>
+  )
+}
