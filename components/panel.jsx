a938ed2ac63151c864074673f069f92df846fec4
Efazul Fayad <efazul.fayad1717@gmail.com>
2023-02-11 18:43:25 +0600

    changes

--- components/panel.jsx
+++ components/panel.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 
@@ -26,9 +27,10 @@ export default function Panel() {
             </div>
           </div>
           <div className="-mt-6 aspect-w-5 aspect-h-3 md:aspect-w-2 md:aspect-h-1">
-            <img
+            <Image
               className="transform translate-x-6 translate-y-6 rounded-md object-cover object-left-top sm:translate-x-16 lg:translate-y-20"
               src="../public/pic.jpeg"
+              alt=" "
             />
           </div>
         </div>
