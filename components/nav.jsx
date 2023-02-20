27e24f74f9aecbe8bb5356166d081a664f93383f
Efazul Fayad <efazul.fayad1717@gmail.com>
2023-02-20 15:00:01 +0600

    fix  20

--- components/nav.jsx
+++ components/nav.jsx
@@ -229,14 +229,14 @@ export default function Nav() {
                   href="#"
                   className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
                 >
-                  Selfcare
+                  Sign up
                 </a>
-                {/* <p className="mt-6 text-center text-base font-medium text-gray-500">
+                <p className="mt-6 text-center text-base font-medium text-gray-500">
                   Existing customer?{' '}
                   <a href="#" className="text-indigo-600 hover:text-indigo-500">
                     Sign in
                   </a>
-                </p> */}
+                </p>
               </div>
             </div>
           </div>
--- components/panel.jsx
+++ components/panel.jsx
@@ -29,7 +29,7 @@ export default function Panel() {
           <div className="-mt-6 aspect-w-5 aspect-h-3 md:aspect-w-2 md:aspect-h-1">
             <Image
               className="transform translate-x-6 translate-y-6 rounded-md object-cover object-left-top sm:translate-x-16 lg:translate-y-20"
-              src="../public/pic.jpeg"
+              src=" "
               alt=" "
             />
           </div>
