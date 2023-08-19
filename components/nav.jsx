e9e606d3a04fc5c328e3f348670caf6d2ce633fb
Kazi Efazul Karim <efazul.fayad1717@gmail.com>
2023-08-19 10:52:05 +0600

    mob fx

--- components/nav.jsx
+++ components/nav.jsx
@@ -247,14 +247,14 @@ export default function Nav() {
                 </div>
                 <div className="mt-6">
                   <a
-                    href="#"
+                    href="register"
                     className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
                   >
                     Sign up
                   </a>
                   <p className="mt-6 text-center text-base font-medium text-gray-500">
                     Existing customer?{' '}
-                    <a href="#" className="text-indigo-600 hover:text-indigo-500">
+                    <a href="login" className="text-indigo-600 hover:text-indigo-500">
                       Sign in
                     </a>
                   </p>
