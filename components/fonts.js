b6ab3e17ad0c09a6e5e9c2848c6b7c3f9a830bbb
Kazi Efazul Karim <efazul.fayad1717@gmail.com>
2023-08-19 10:47:24 +0600

    font

--- components/fonts.js
+++ components/fonts.js
@@ -4,7 +4,7 @@ const Fonts = () => {
   return (
     <Global
       styles={`
-    @import url("https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@500&family=Roboto:wght@100&display=swap");
+    @import url("https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@500&family=Roboto:wght@100;400&display=swap" rel="stylesheet");
      `}
     />
   )
