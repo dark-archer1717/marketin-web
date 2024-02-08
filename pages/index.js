06c9c9bf78be24a4189ed33758b895ff327dfd3b
Kazi Efazul Karim <efazul.fayad1717@gmail.com>
2024-02-08 14:57:13 +0600

    Update favicon image

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <title>Sohag Technology - High Speed Internet</title>
         <meta name="description" content="Leading ISP in Bangladesh" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/logo.webp" />
+        <link rel="icon" href="/logo2.webp" />
       </Head>
 
       <Nav />
